16c8a19a8c668a833e909037852b0fa569765786
Vaishali Tomar <vaishalitomar830@gmail.com>
2024-08-30 00:11:48 +0530

    done

--- Backend/index.js
+++ Backend/index.js
@@ -1,1 +1,23 @@
-console.log("hii hello");
+import express from "express";
+
+const app =express();
+
+app.get("/", (req, res, next) => {
+    console.log("A");
+ return next(new Error("Asheai"));
+}, (req, res, next) => {
+  console.log("B");
+
+}, (req, res, next)=> {
+    console.log("C");
+})
+
+app.use((err, req, res, next) => {
+    res.status(404).json({
+        message: err.message,
+    })
+})
+
+app.listen(4000, () => {
+    console.log(`Server is wprking with port 4000`);
+})
--- ClintSide/src/App.jsx
+++ ClintSide/src/App.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from 'react';
+import "./App.css"; // Ensure Tailwind directives are included here
 
 const App = () => {
   return (
-    <div>
-      main file
+    <div className='bg-blue-100 h-screen flex items-center justify-center'>
+      <h1 className='text-4xl font-bold text-gray-800'>Main File</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
